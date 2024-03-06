d5d3c93da46776b3dfdf465825b2447b9841e201
Macarinschi Andrei <andreimakarinskii@gmail.com>
2024-03-06 23:33:52 +0200

    new update

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -3,29 +3,53 @@
 
 using namespace std;
 
-enum class Genre { 
-	
-	Classics, 
-	Novel, 
+const int MAX_SIZE = 30;
+
+enum class Genre {
+
+	Classics,
+	Novel,
 	Detective,
-	Fantasy, 
-	Horror, 
-	History 
+	Fantasy,
+	Horror,
+	History
 
 };
 
 struct Book {
 
-	array<char, 30> title;
+	array<char, MAX_SIZE> title;
 	int pages;
 	Genre genre;
 
 };
 
+void serializeTitle(const Book& book, ostream& os) {
+
+	os << book.title.data();
+}
+
+void serializePages(const Book& book, ostream& os) {
+
+	os << book.pages;
+}
+
+void serializeGenre(const Book& book, ostream& os) {
+
+	os << static_cast<int>(book.genre);
+}
+
+void serializeBook(const Book& book, ostream& os) {
+
+	serializeTitle(book, os);
+	os << ", ";
+	serializePages(book, os);
+	os << ", ";
+	serializeGenre(book, os);
+	os << "\n";
+}
 
 int main() {
-
-
 
 	return 0;
 }
