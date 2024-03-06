fc68fc0f09a55a1c37eb06e9eb8810cc21771955
Macarinschi Andrei <andreimakarinskii@gmail.com>
2024-03-06 22:02:41 +0200

    lab2 update

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <array>
 
 using namespace std;
+
+enum class Genre { 
+	
+	Classics, 
+	Novel, 
+	Detective,
+	Fantasy, 
+	Horror, 
+	History 
+
+};
+
+struct Book {
+
+	array<char, 30> title;
+	int pages;
+	Genre genre;
+
+};
+
 
 int main() {
 
